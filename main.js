2845587b71c308e2118c9014eae8da156309f80a
reza <rezasemyari1991@gmail.com>
2020-06-19 17:49:21 +0430

    add grid bootstrap

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow.setFullScreen(true);
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.maximize();
 
   // and load the index.html of the app.
   win.loadFile('index.html')
@@ -10,24 +18,4 @@ function createWindow () {
 
 app.whenReady().then(createWindow)
 
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+console.log('ohoishdf');
